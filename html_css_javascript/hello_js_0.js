76d22d95b438bab8c500913d216309166210925c
onkiratu <nkiratu@gmail.com>
2024-01-05 02:45:22 +0300

    html_css_js

---
+++ html_css_javascript/hello_js_0.js
@@ -0,0 +1,10 @@
+// Separate JS
+
+
+                document.addEventListener('DOMContentLoaded', function(){
+                    document.querySelector('form').addEventListener('submit', function(event) {
+                        let name = document.querySelector('#name').value;
+                        alert('hello, ' + name);
+                        event.preventDefault();
+                    });
+                });
